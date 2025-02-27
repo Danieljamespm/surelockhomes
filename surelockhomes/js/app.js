8496d93bb1805bcc78a76a19619f38ef9a23c1ce
Danny Mettler <danieljamespm@gmail.com>
2025-02-27 11:15:25 -0500

    added scroll effect to main hero section.

--- surelockhomes/js/app.js
+++ surelockhomes/js/app.js
@@ -14,3 +14,21 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll('.hidden')
 hiddenElements.forEach((el) => observer.observe(el))
+
+
+
+
+const observer2 = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting){
+            entry.target.classList.add('show-2')
+        } else{
+            // entry.target.classList.remove('show')
+        }
+    })
+})
+
+
+const hiddenElements2 = document.querySelectorAll('.hidden-2')
+hiddenElements2.forEach((el) => observer.observe(el))
